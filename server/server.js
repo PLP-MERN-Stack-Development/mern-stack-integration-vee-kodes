92cf4579b3d9bf9b4817e1be17c5e302a179223f
vee-kodes <violachepchirchir23@gmail.com>
2025-10-27 13:13:49 +0300

    fix: updated CORS configuration in server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://devdiaryblog.netlify.app/"],
+  credentials: true,
+})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
